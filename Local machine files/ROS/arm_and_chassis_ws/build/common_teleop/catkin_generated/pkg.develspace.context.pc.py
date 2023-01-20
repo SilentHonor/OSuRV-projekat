acd27f800d5a33af726c5cad71d45bc8577f76a2
Veljko Bogdanović <37028568+SilentHonor@users.noreply.github.com>
2023-01-20 10:59:15 -0600

    Add files via upload

---
+++ Local machine files/ROS/arm_and_chassis_ws/build/common_teleop/catkin_generated/pkg.develspace.context.pc.py
@@ -0,0 +1,8 @@
+# generated from catkin/cmake/template/pkg.context.pc.in
+CATKIN_PACKAGE_PREFIX = ""
+PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/usr/include/eigen3".split(';') if "/usr/include/eigen3" != "" else []
+PROJECT_CATKIN_DEPENDS = "rospy;roscpp;geometry_msgs;tf;ackermann_msgs;nav_msgs".replace(';', ' ')
+PKG_CONFIG_LIBRARIES_WITH_PREFIX = "-lcommon_teleop".split(';') if "-lcommon_teleop" != "" else []
+PROJECT_NAME = "common_teleop"
+PROJECT_SPACE_DIR = "/home/pi/Documents/OSuRV_Project/ROS/arm_and_chassis_ws/devel"
+PROJECT_VERSION = "0.1.0"
